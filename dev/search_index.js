781d8095e498720d56ec5b1c4b454b6a9d918d7a
Documenter.jl <documenter@juliadocs.github.io>
2021-04-08 16:22:26 +0000

    build based on 4a76805

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SharpArithmetic","category":"page"},{"location":"#SharpArithmetic","page":"Home","title":"SharpArithmetic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SharpArithmetic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SharpArithmetic]","category":"page"}]
+}
